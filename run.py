eee2524322d6e9bf7bdb6015b17565506dce464a
David Ward <davidward1307@gmail.com>
2022-04-25 19:21:48 +0100

    wrote out print statements that will work as a main menu

--- run.py
+++ run.py
@@ -1,7 +1,18 @@
 from tkinter import N
 
+boardSize = 5
+shipNum = 4
 
-def createMenu():
-    print("-" * 35)
-    print("Welcome to BATTLE BOATS")
 
+def Menu():
+    print("           "+"-" * 35)
+    print("               Welcome to BATTLE BOATS")
+    print(f"           board size: {boardSize}, number of ships: {shipNum}")
+    print("           " +"-" * 35)
+    print(f"q: start game    w: change Board size    e: Change Ship Number \n")
+
+    option = input("Please choose and option").lower()
+
+    print(option)
+
+Menu()
