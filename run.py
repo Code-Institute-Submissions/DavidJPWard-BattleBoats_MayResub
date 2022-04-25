d9ffcea65aa604e12cdee7980a0e28044bbb76a2
David Ward <davidward1307@gmail.com>
2022-04-25 19:53:15 +0100

    added function to validate integers, added function to change board size

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from multiprocessing.sharedctypes import Value
 from tkinter import N
 
 boardSize = 5
@@ -11,8 +12,33 @@ def Menu():
     print("           " +"-" * 35)
     print(f"q: start game    w: change Board size    e: Change Ship Number \n")
 
-    option = input("Please choose and option").lower()
+    option = input("Please choose and option: ").lower()
 
-    print(option)
+    if(option == "q"):
+        print("q")
+    elif(option == "w"):
+        changeBoardSize()
+    elif(option == "e"):
+        changeShipNumber()
+    else:
+        print("Invalid option, please pick again")
+
+def changeBoardSize():
+    print("please pick a number between 4 and 10 to change the game board")
+
+def validate_int(input, length, min_value, max_value):
+    try:
+        int(input)
+        if(len(input)>length):
+            raise ValueError(f"input has exceeded the maximum length of {length} chracters")
+        if(input < min_value):
+            raise ValueError(f"input cannot be less than {min_value}")
+        if(input > max_value):
+            raise ValueError(f"input cannot be more than {max_value}")
+    except ValueError as e:
+        print(f"invalid input, please input an integer")
+        return False;
+    return True
+
 
 Menu()
