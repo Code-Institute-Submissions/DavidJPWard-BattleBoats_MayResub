652afefd168872cc621156877fc0f0f5166d3fe2
David Ward <davidward1307@gmail.com>
2022-04-26 18:36:17 +0100

    added function to change board size

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from multiprocessing.sharedctypes import Value
+from pickle import TRUE
 from tkinter import N
 
 boardSize = 5
@@ -17,27 +18,38 @@ def Menu():
     if(option == "q"):
         print("q")
     elif(option == "w"):
-        changeBoardSize()
+        boardSize = changeBoardSize()
+        Menu()
     elif(option == "e"):
         changeShipNumber()
     else:
         print("Invalid option, please pick again")
 
 def changeBoardSize():
-    print("please pick a number between 4 and 10 to change the game board")
+    print("please pick a number between 4 and 10 to change the game board\n")
+    while True:
+        new_size = input()
+        if(validate_int(new_size, 2, 4, 10)):
+            break
+        print("Please Pick Again: ")
+    return new_size()
+
+
 
 def validate_int(input, length, min_value, max_value):
     try:
-        int(input)
-        if(len(input)>length):
+        if(str(input).isdigit() != True):
+            raise ValueError(f"input must be an integer")
+        input = int(input)
+        if(len(str(input)) > length):
             raise ValueError(f"input has exceeded the maximum length of {length} chracters")
         if(input < min_value):
             raise ValueError(f"input cannot be less than {min_value}")
         if(input > max_value):
             raise ValueError(f"input cannot be more than {max_value}")
     except ValueError as e:
-        print(f"invalid input, please input an integer")
-        return False;
+        print(f"invalid input, {e}\n")
+        return False
     return True
 
 
